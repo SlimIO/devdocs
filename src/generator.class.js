d3b07706956fcac1d6c8d8719971b363b2a90351
markobobby <malaj1.mark@gmail.com>
2019-06-01 11:00:41 +0200

    chore: manage content.default object

--- src/generator.class.js
+++ src/generator.class.js
@@ -10,7 +10,7 @@ const semver = require("semver");
 const zup = require("zup");
 
 // CONSTANTS
-TEMPLATE_DIR = join(__dirname, "..", "view", "template");
+const TEMPLATE_DIR = join(__dirname, "..", "view", "template");
 
 /**
  * @class Generator
@@ -35,6 +35,9 @@ class Generator {
             throw new Error("the 'members' or 'orphans' properties of the 'docs' argument are missing");
         }
         const { orphans, members } = docs;
+
+        this.VALID_PROP = new Set(["desc", "example", "defaultVals"]);
+
         this.members = members;
         this.orphans = orphans;
         this.zupObject = {};
@@ -81,7 +84,6 @@ class Generator {
             typeReturn,
             content
         } = options;
-        let { version = "0.1.0" } = options;
         if (!is.boolean(isStatic)) {
             throw new TypeError("isStatic param must be a type of boolean");
         }
@@ -91,9 +93,7 @@ class Generator {
         if (!is.string(typeReturn)) {
             throw new TypeError("typeReturn param must be a type of string");
         }
-        if (!is.string(version)) {
-            throw new TypeError("version param must be a type of string");
-        }
+
         if (!is.undefined(content) && !is.plainObject(content)) {
             throw new TypeError("content param must be a type of Object");
         }
@@ -101,12 +101,17 @@ class Generator {
             throw new Error("content must not be an empty Object");
         }
         // Verify if content contains at least one of the required properties
-        const validProps = new Set(["desc", "example"]);
         const properties = Object.keys(content);
-        const containValidProp = properties.some((item) => validProps.has(item));
+        const containValidProp = properties.some((item) => this.VALID_PROP.has(item));
         if (!containValidProp) {
-            throw new Error(`content must contain at least one of these properties : ${Array.from(validProps).join(", ")}`);
+            throw new Error(`content must contain at least one of these properties : ${Array.from(this.VALID_PROP).join(", ")}`);
         }
+
+        let { version = "0.1.0" } = options;
+        if (!is.string(version)) {
+            throw new TypeError("version param must be a type of string");
+        }
+        // Check if the version complies with the semver standards
         const { version: coerceVersion } = semver.coerce(version) || { version };
         version = coerceVersion;
         if (!semver.valid(version)) {
@@ -115,7 +120,40 @@ class Generator {
         if (!is.string(content.desc)) {
             throw new TypeError("desc property must be a type of String");
         }
-        // console.log(params);
+
+        if (Object.prototype.hasOwnProperty.call(content, "defaultVals")) {
+            if (!is.array(content.defaultVals)) {
+                throw new TypeError("defaultVals must be a type of Array<Object>, check doc");
+            }
+            if (content.defaultVals.length === 0) {
+                throw new Error("defaultVals must not be an empty Array");
+            }
+            // eslint-disable-next-line prefer-const
+            for (let [i, { name, type, value }] of content.defaultVals.entries()) {
+                if (is.undefined(name) || is.undefined(type) || is.undefined(value)) {
+                    throw new Error("DefaultVals is an array of Object, each object must contain these keys : name, type and value");
+                }
+
+                if (!is.string(name)) {
+                    throw new TypeError(`defaultVals.name must be a type of String
+                    Look the ${i} element of content.Default array`);
+                }
+                if (!is.string(type)) {
+                    throw new TypeError("defaultVals.type must be a type of String");
+                }
+                type = type.toLowerCase();
+                if (type === "string" || type === "boolean" || type === "number") {
+                    content.defaultVals[i].type = type.toLowerCase();
+                }
+                else {
+                    content.defaultVals[i].type = "obj";
+                }
+                if (is.plainObject(value)) {
+                    content.defaultVals[i].value = JSON.stringify(value);
+                }
+            }
+        }
+
         const methodTemplate = zup(this.basicHtmlMethod)({
             name,
             isStatic,
@@ -163,9 +201,17 @@ generator.createMethod("blabla", {
     content: {
         foo: "fff",
         bar: "bbb",
+        foobar: "foobar",
         desc: "desc of the method",
-        example: "const foo = true",
-        foobar: "foobar"
+        defaultVals: [
+            { name: "nameArg", type: "String", value: "default value" },
+            { name: "isSecure", type: "boolean", value: true },
+            { name: "nbTime", type: "number", value: 50 },
+            { name: "name3", type: "Payload", value: "Payload" },
+            { name: "name5", type: "Other", value: [25, 50] },
+            { name: "name5", type: "Other", value: { name: "test" } }
+        ],
+        example: "const foo = true"
     }
 
 });
